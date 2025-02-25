823a16b45d2bff136f3ae85d8e4311a752c79878
Carson <carson@mcmumf.dev>
2025-02-24 22:48:13 -0500

    move some files around for better organization

--- talos/build-configs.sh
+++ talos/build-configs.sh
@@ -3,7 +3,9 @@ talosctl gen config --force \
     --talos-version 1.9.0\
     --kubernetes-version 1.32.0 \
     --with-secrets secrets.yaml \
-    juicecloud https://k8s.mcmumf.dev:6443 
+    juicecloud https://k8s.mcmumf.dev:6443 \
+    --config-patch-control-plane @patches/mayastor-patch-cp.yaml \
+    --config-patch-worker @patches/mayastor-patch.yaml
 
 # hl1
 talosctl machineconfig patch controlplane.yaml \
@@ -12,13 +14,13 @@ talosctl machineconfig patch controlplane.yaml \
     --output outputs/hl1.yaml
 
 # hl2
-talosctl machineconfig patch controlplane.yaml \
-    --patch @patches/common.yaml \
+talosctl machineconfig patch worker.yaml \
     --patch @nodes/hl2.yaml \
     --output outputs/hl2.yaml
 
-# hl3
+# hl3 (control plane and storage)
 talosctl machineconfig patch controlplane.yaml \
     --patch @patches/common.yaml \
+    --patch @patches/mayastor-patch.yaml \
     --patch @nodes/hl3.yaml \
     --output outputs/hl3.yaml
