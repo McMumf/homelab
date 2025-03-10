27523192d5366de2c538ced629c5da5ffc8a8fa4
Carson <carson@mcmumf.dev>
2025-03-09 22:26:42 -0400

    Use the proper output this time

--- talos/build-configs.sh
+++ talos/build-configs.sh
@@ -6,39 +6,39 @@ talosctl gen config --force \
     --output-dir _out
 
 # apollo (control-plane)
-talosctl machineconfig patch controlplane.yaml \
+talosctl machineconfig patch _out/controlplane.yaml \
     --patch @patches/common.yaml \
     --patch @nodes/apollo.yaml \
     --output _out/apollo.yaml
 
 # artemis (control-plane)
-talosctl machineconfig patch controlplane.yaml \
+talosctl machineconfig patch _out/controlplane.yaml \
     --patch @patches/common.yaml \
     --patch @nodes/artemis.yaml \
     --output _out/artemis.yaml
 
 # skylab (control-plane)
-talosctl machineconfig patch controlplane.yaml \
+talosctl machineconfig patch _out/controlplane.yaml \
     --patch @patches/common.yaml \
     --patch @nodes/skylab.yaml \
     --output _out/skylab.yaml
 
 # columbia (worker and storage)
-talosctl machineconfig patch worker.yaml \
+talosctl machineconfig patch _out/worker.yaml \
     --patch @patches/common.yaml \
     --patch @patches/mayastor-patch.yaml \
     --patch @nodes/columbia.yaml \
     --output _out/columbia.yaml
 
 # atlantis (worker and storage)
-talosctl machineconfig patch worker.yaml \
+talosctl machineconfig patch _out/worker.yaml \
     --patch @patches/common.yaml \
     --patch @patches/mayastor-patch.yaml \
     --patch @nodes/atlantis.yaml \
     --output _out/atlantis.yaml
 
 # challenger (worker and storage)
-talosctl machineconfig patch worker.yaml \
+talosctl machineconfig patch _out/worker.yaml \
     --patch @patches/common.yaml \
     --patch @patches/mayastor-patch.yaml \
     --patch @nodes/challenger.yaml \
