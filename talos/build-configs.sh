291206608a2e52f20efa009fa1d264af32e507ae
Carson <carson@mcmumf.dev>
2025-05-14 09:51:39 -0400

    Talos v1.10.1

--- talos/build-configs.sh
+++ talos/build-configs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 talosctl gen config --force \
-    --talos-version 1.9.5 \
+    --talos-version 1.10.1 \
     --kubernetes-version 1.33.0 \
     homelab https://192.168.5.100:6443 \
     --config-patch @patches/cni.yaml \
